cd2adcdb59f35a27d626f361e2575c10a1634539
Dylan Cagnotti <106975297+dylancagnotti@users.noreply.github.com>
2024-01-11 23:46:30 +0100

    feat: creted GetDotAttributes method in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     private EventManager eventManager;
 
+    public DotAttributes GetDotAttributes(GameObject dot)
+    {
+        Sprite mainAbilitySprite = null;
+        Sprite secondaryAbilitySprite = null;
+        if (dot.GetComponent<PlayerAbilitiesBase>() is QuickDotAbilities quickDot)
+        {
+            mainAbilitySprite = Resources.Load<Sprite>("Sprites/ArrowIcon");
+            secondaryAbilitySprite = Resources.Load<Sprite>("Sprites/SpeedIcon");
+            return new DotAttributes(name: "QuickDot", color: "#FFFFFF", mainAbilityName: "Arrow", secondaryAbilityName: "Speed", mainAbilitySprite, secondaryAbilitySprite);
+        }
+        else if (dot.GetComponent<PlayerAbilitiesBase>() is FireDotAbilities fireDot)
+        {
+            mainAbilitySprite = Resources.Load<Sprite>("Sprites/ArrowIcon");
+            secondaryAbilitySprite = Resources.Load<Sprite>("Sprites/SpeedIcon");
+            return new DotAttributes(name: "FireDot", color: "#FF2C00", mainAbilityName: "FIreball", secondaryAbilityName: "Firetrail", mainAbilitySprite, secondaryAbilitySprite);
+        }
+        else if (dot.GetComponent<PlayerAbilitiesBase>() is MagicDotAbilities magicDot)
+        {
+            mainAbilitySprite = Resources.Load<Sprite>("Sprites/ArrowIcon");
+            secondaryAbilitySprite = Resources.Load<Sprite>("Sprites/SpeedIcon");
+            return new DotAttributes(name: "MagicDot", color: "#FFA7F9", mainAbilityName: "Magic Ball", secondaryAbilityName: "Shape", mainAbilitySprite, secondaryAbilitySprite);
+        }
+        else if (dot.GetComponent<PlayerAbilitiesBase>() is PoisonDotAbilities poisonDot)
+        {
 
+            mainAbilitySprite = Resources.Load<Sprite>("Sprites/ArrowIcon");
+            secondaryAbilitySprite = Resources.Load<Sprite>("Sprites/SpeedIcon");
+            return new DotAttributes(name: "PoisonDot", color: "#00E852", mainAbilityName: "Dart", secondaryAbilityName: "Poison Gas", mainAbilitySprite, secondaryAbilitySprite);
+        }
+        else
+        {
+            mainAbilitySprite = Resources.Load<Sprite>("Sprites/ArrowIcon");
+            secondaryAbilitySprite = Resources.Load<Sprite>("Sprites/SpeedIcon");
+            return new DotAttributes(name: "Dot", color: "#CCCCCC", mainAbilityName: "None", secondaryAbilityName: "None", mainAbilitySprite, secondaryAbilitySprite);
+        };
+    }
     private void Awake()
     {
         eventManager = GetComponent<EventManager>();
