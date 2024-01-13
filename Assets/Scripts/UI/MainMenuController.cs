8fbe88dfc364726f33f95669b4e83b3d93ee18c2
Dylan Cagnotti <106975297+dylancagnotti@users.noreply.github.com>
2024-01-13 16:49:42 +0100

    feat: updated main menu controller

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
-    public Button playButton;
-    public Button soundButton;
-    public Button aboutButton;
     public Slider soundSlider;
+
+    public enum DotType
+    {
+        White,
+        Toxin,
+        Magic,
+        Pyre
+    }
+
+
+    public void Start()
+    {
+        //set the value of the sound slider to the current volume
+        soundSlider.value = AudioListener.volume;
+    }
 
     public void HandleSoundButtonClick()
     {
@@ -19,7 +32,8 @@ public class MainMenuController : MonoBehaviour
     public void HandlePlayButtonClick()
     {
         //load the dot selection scene
-        UnityEngine.SceneManagement.SceneManager.LoadScene("DotSelection");
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void HandleSoundSliderValueChange()
     {
@@ -31,5 +45,25 @@ public class MainMenuController : MonoBehaviour
     {
         //load the about scene
         UnityEngine.SceneManagement.SceneManager.LoadScene("About");
+    }
+
+    public void HandleBackButtonClick()
+    {
+        //load the previous scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void SetSelectedDot(string dotType)
+    {
+        //set the selected dot type
+        Debug.Log(dotType);
+        PlayerPrefs.SetString("SelectedDot", dotType);
+    }
+
+    public void SetSelectedMap(int map)
+    {
+        //set the selected map
+        Debug.Log(map);
+        PlayerPrefs.SetInt("SelectedMap", map);
     }
 }
